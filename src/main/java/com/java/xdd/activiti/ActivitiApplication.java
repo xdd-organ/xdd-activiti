c579b7970ba53a99f9a77fa1ab54acd85ffb503b
xdd <767403510@qq.com>
2019-10-16 10:57:06 +0800

    初始化应用

---
+++ src/main/java/com/java/xdd/activiti/ActivitiApplication.java
@@ -0,0 +1,15 @@
+package com.java.xdd.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xdd
+ * @date 2019/10/16
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
